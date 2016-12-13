d63623ff474305d5d55b14d69ae0e9701150ef34
poltts <poltss@gmail.com>
2016-12-13 11:54:12 -0200

    update tela 4 and add tela 2

---
+++ tela-02/legenda.js
@@ -0,0 +1,16 @@
+var legendas =[
+{
+	idioma:"português",
+	legendas:[
+		{
+			tempoInicial:"00:01,00",
+			tempoFinal:"03:00,00",
+			texto:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+		},
+		{
+			tempoInicial:"03:01,00",
+			tempoFinal:"03:25,00",
+			texto:"Another orem Ipsum is simply dummy text of the printing and typesetting industry."
+		}
+	]},
+]
