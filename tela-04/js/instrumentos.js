fa84113a9068113c56ad72433214695457b6f75b
poltts <poltss@gmail.com>
2016-12-14 15:00:26 -0200

    add instruments layout and update instruments style

--- tela-04/js/instrumentos.js
+++ tela-04/js/instrumentos.js
@@ -1,26 +1,55 @@
 $(function(){ 
- 
-    var current = false; 
 
-   console.log($(".instrumentos"));
+   $('.desc').hide();
+    
+    var instruments = {
+        violino: {
+            nome: 'violino',
+            tipo: 'cordas'
+        },
+        contrabaixo: {
+            nome: 'contrabaixo',
+            tipo: 'cordas'
+        },
+        tromba: {
+            nome: 'trompa',
+            tipo: 'metais'
+        },
+        clarinete: {
+            nome: 'clarinete',
+            tipo: 'madeiras'
+        },
+        timpano: {
+            nome: 'tímpanos',
+            tipo: 'percussão'
+        }
+    };  
+    
     $(".instrumentos > div").bind("mouseenter", function(event){
         event.preventDefault(); 
-                           
+                      
         var element     = this.id;
         var div         = '#' + element;
-        var instrumento = $('audio'+div).get(0);
+        var instrumento = $(div+' > audio').get(0);
         var $stage      = $('.instrumentos > div'+div);
         var sprite      = event.target;
 
         var animation   = new Motio(sprite, {fps: 15, frames: 9, startPaused: true}); 
+        var descricao = instruments[element].tipo + ' - '+instruments[element].nome;
+        console.log(element);   
+        
+        $('.desc').show();
+        $('.desc').html('<p>'+descricao+'</p>');
 
         instrumento.play();
-        animation.play();  
+        // animation.play();  
 
             $stage.on("mouseleave", function(e){
                 e.preventDefault(); 
                 instrumento.pause();
-                animation.pause(); 
+                $('.desc').html('<p></p>');  
+                $('.desc').hide();
+                // animation.pause(); 
             });
 
         return false; 
